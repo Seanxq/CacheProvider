acad28cb1c612d12751b560fc45c0fbe46176f79
Chad <chadit@gmail.com>
2014-11-06 15:01:59 -0500

    started work ona  multiple cache provider

---
+++ src.test/CacheProviderMultiTests/MultiCacheProviderTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Specialized;
+using CacheProvider.Multi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CacheProviderMultiTests
+{
+    [TestClass]
+    public class MultiCacheProviderTests
+    {
+        private MultiCacheProvider _cacheProvider;
+
+        private readonly NameValueCollection _enabledSettings = new NameValueCollection
+            {
+                {"application", "Cache"},
+                {"timeout", "5"},
+                {"host", "127.0.0.1"},
+                {"port","27017"},
+                {"env", "UnitTest"},
+                {"providers", "MemoryCacheProvider, MongoCacheProvider"}
+            };
+
+        private readonly NameValueCollection _disabledSettings = new NameValueCollection
+            {
+                {"application", "Cache"},
+                {"timeout", " 5"},
+                {"host", "127.0.0.1"},
+                {"port","27017"},
+                {"env", "UnitTest"},
+                {"enable", "false"},
+                {"providers", "MemoryCacheProvider, MongoCacheProvider"}
+            };
+
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/CacheProvider.Multi/MultiCacheProvider.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Threading.Tasks;
+using CacheProvider.Interface;
+
+namespace CacheProvider.Multi
+{
+    public class MultiCacheProvider: CacheProvider
+    {
+        private bool _isEnabled;
+        private List<ICacheProvider> _cacheProviders; 
+
+        /// <summary>
+        ///     Initialize from config
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="config">Config properties</param>
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            base.Initialize(name, config);
+           
+            _isEnabled = true;
+            var enabled = config["enable"];
+            if (enabled == null)
+            {
+                _isEnabled = true;
+            }
+            else
+            {
+                bool.TryParse(config["enable"], out _isEnabled);
+            }
+
+            var providers = config["providers"];
+            if (providers == null)
+            {
+                throw new ConfigurationErrorsException("Missing providers list.. example memory,mongo");
+            }
+
+            foreach (var c in providers.Split(','))
+            {
+                var item = (ICacheProvider) System.Reflection.Assembly.GetExecutingAssembly().CreateInstance(c);
+
+                _cacheProviders.Add(item);
+            }
+
+
+        }
+
+
+        /*
+         public interface IPreparer
+    {
+        void Run(Dictionary<string, object> data);
+    }
+
+    public interface IPreparerFactory : IFactory<IPreparer>
+    {
+        IPreparer GetInstance(string key);
+    }
+
+    public class UnityPreparerFactory : UnityFactory<IPreparer>, IPreparerFactory
+    {
+        public UnityPreparerFactory() : base(ServiceLayerConstants.DEFAULT_CONTAINER_NAME) { }
+
+        IPreparer IPreparerFactory.GetInstance(string key)
+        {
+            return base.GetInstance(key.ToLower());
+        }
+    }
+          
+         */
+
+
+        public override Task<object> Get(object cacheKey, string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<T> Get<T>(object cacheKey, string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> Exist(object cacheKey, string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> Add(object cacheKey, object cacheObject, string region, int expirationInMinutes = 15)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> Add(object cacheKey, object cacheObject, string region, bool allowSliddingTime, int expirationInMinutes = 15)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> AddPermanent(object cacheKey, object cacheObject, string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> Remove(object cacheKey, string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> RemoveAll(string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> RemoveExpired(string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<long> Count(string region)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
